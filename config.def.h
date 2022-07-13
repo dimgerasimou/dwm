1caa9e4b3f56f6a3dc737d71a7846450beb1a61c
Dimitris Gerasimou <gerasimou@ece.auth.gr>
2022-07-13 18:23:31 +0300

    center patch

--- config.def.h
+++ config.def.h
@@ -48,12 +48,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title            tags mask    isfloating    isterminal    noswallow    monitor */
-	{ "Gimp",     NULL,       NULL,            0,           1,            0,            0,           -1 },
-	{ "Firefox",  NULL,       NULL,            1 << 8,      0,            0,            -1,          -1 },
-	{ "Dolphin",  NULL,       NULL,            0,           1,            0,            0,           -1 },
-	{ "Alacritty",NULL,       NULL,            0,           0,            1,            0,           -1 },
-	{ NULL,       NULL,       "Event Tester",  0,           0,            0,            1,           -1 },
+	/* class      instance    title            tags mask    iscentered    isfloating    isterminal    noswallow    monitor */
+	{ "Gimp",     NULL,       NULL,            0,           0,            1,            0,            0,           -1 },
+	{ "Firefox",  NULL,       NULL,            1 << 8,      1,            0,            0,            -1,          -1 },
+	{ "Dolphin",  NULL,       NULL,            0,           0,            1,            0,            0,           -1 },
+	{ "Alacritty",NULL,       NULL,            0,           0,            0,            1,            0,           -1 },
+	{ NULL,       NULL,       "Event Tester",  0,           0,            0,            0,            1,           -1 },
 };
 
 /* layout(s) */
