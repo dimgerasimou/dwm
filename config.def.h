d666388c8594cb742cf63e2f79ffd6dcb4148d05
Dimitris Gerasimou <gerasimou@ece.auth.gr>
2022-07-18 22:05:43 +0300

    cyclelayouts patch

--- config.def.h
+++ config.def.h
@@ -78,6 +78,7 @@ static const Layout layouts[] = {
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "HHH",      grid },
+	{ NULL,       NULL },
 };
 
 /* key definitions */
@@ -122,9 +123,13 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return,     zoom,           {0} },
 	{ MODKEY,                       XK_Tab,        view,           {0} },
 	{ MODKEY|ShiftMask,             XK_BackSpace,  killclient,     {0} },
+	
+	/* layouts */
 	{ MODKEY,                       XK_t,          setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,          setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,          setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ControlMask,           XK_comma,      cyclelayout,    {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_period,     cyclelayout,    {.i = +1 } },	
 	{ MODKEY,                       XK_g,          setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,      setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,      togglefloating, {0} },
