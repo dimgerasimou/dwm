ce206bae08fb16ba48065824a743c5fcfa143826
Dimitris Gerasimou <gerasimou@ece.auth.gr>
2022-07-19 00:22:13 +0300

    Changed settings

--- build/config.def.h
+++ build/config.def.h
@@ -1,18 +1,26 @@
-/*  ____   ____                                          */
-/* |  _ \ / ___|    Dimitris Gerasimou (dimgerasimou)    */
-/* | | | | |  _     <https://github.com/dimgerasimou>    */
-/* | |_| | |_| |                                         */
-/* |____/ \____|                                         */
-/*                                                       */
-/* My dwm configuration file.                            */
+/*  ____   ____                                           */
+/* |  _ \ / ___|    Dimitris Gerasimou (dimgerasimou)     */
+/* | | | | |  _     <https://github.com/dimgerasimou>     */
+/* | |_| | |_| |                                          */
+/* |____/ \____|                                          */
+/*                                                        */
+/* My dwm configuration file.                             */
 
-/* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+/* Appearance --------------------------------------------*/
+
+/* borders */
+static const unsigned int borderpx  = 1;                        /* border pixel of windows */
+static const unsigned int snap      = 32;                       /* snap pixel */
+
+/* bar */
+static const int showbar            = 1;                        /* 0 means no bar */
+static const int topbar             = 1;                        /* 0 means bottom bar */
+
+/* fonts */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
+
+/* colors */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -24,7 +32,9 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
-/* tagging */
+/* Functionality -----------------------------------------*/
+
+/* tags */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
@@ -34,19 +44,19 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const float mfact     = 0.55;                           /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;                              /* number of clients in master area */
+static const int resizehints = 1;                              /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1;                           /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[]=",      tile },                                      /* first entry is default */
+	{ "><>",      NULL },                                      /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
@@ -61,25 +71,41 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+/* Commands ----------------------------------------------*/
+
+/* application launchers */
+static char dmenumon[2] = "0";                                 /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]    = { "alacritty", NULL };
+static const char *browsercmd[] = { "firefox", NULL };
+static const char *emailcmd[]   = { "thunderbird", NULL };
+static const char *fmgercmd[]   = { "dolphin", NULL };
+
+/* Keybinds ----------------------------------------------*/
 
 static Key keys[] = {
 	/* modifier                     key            function        argument */
+
+	/* application control */
 	{ MODKEY,                       XK_p,          spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return,     spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return,     spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_w,          spawn,          {.v = browsercmd } },
+	{ MODKEY,                       XK_e,          spawn,          {.v = emailcmd } },
+	{ MODKEY,                       XK_d,          spawn,          {.v = fmgercmd } },
+	{ MODKEY|ShiftMask,             XK_BackSpace,  killclient,     {0} },
+
+
 	{ MODKEY,                       XK_b,          togglebar,      {0} },
+
+	/* window controls */
 	{ MODKEY,                       XK_j,          focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,          focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,          incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,          incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,          setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,          setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return,     zoom,           {0} },
+	/*{ MODKEY,                       XK_Return,     zoom,           {0} },*/
 	{ MODKEY,                       XK_Tab,        view,           {0} },
-	{ MODKEY|ShiftMask,             XK_BackSpace,  killclient,     {0} },
 	{ MODKEY,                       XK_t,          setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,          setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,          setlayout,      {.v = &layouts[2]} },
@@ -91,6 +117,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period,     focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,      tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period,     tagmon,         {.i = +1 } },
+
+	/* tagkeys */
 	TAGKEYS(                        XK_1,                          0)
 	TAGKEYS(                        XK_2,                          1)
 	TAGKEYS(                        XK_3,                          2)
@@ -100,6 +128,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                          6)
 	TAGKEYS(                        XK_8,                          7)
 	TAGKEYS(                        XK_9,                          8)
+
+	/* login */
 	{ MODKEY|ShiftMask,             XK_q,          quit,           {0} },
 };
 
