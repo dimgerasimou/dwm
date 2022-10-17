152e84a90682c9fe3c57e106c5bad742a24c3495
Dimitris Gerasimou <gerasimou@ece.auth.gr>
2022-10-18 02:21:25 +0300

    Changed settings

--- build/config.h
+++ build/config.h
@@ -58,13 +58,22 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *dmenucmd[]   = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]    = { "alacritty", NULL };
+static const char *browsercmd[] = { "firefox", NULL };
+static const char *emailcmd[]   = { "thunderbird", NULL };
+static const char *explrcmd[]   = { "dolphin", NULL };
 
+/* keys */
 static const Key keys[] = {
 	/* modifier                     key           function        argument */
+	/* spawn */
 	{ MODKEY,                       XK_p,         spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return,    spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_w,         spawn,          {.v = browsercmd } },
+	{ MODKEY,                       XK_e,         spawn,          {.v = emailcmd } },
+	{ MODKEY,                       XK_d,         spawn,          {.v = explrcmd } },
+
 	{ MODKEY,                       XK_b,         togglebar,      {0} },
 	{ MODKEY,                       XK_j,         focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,         focusstack,     {.i = -1 } },
