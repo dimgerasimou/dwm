d77e982b6b39b93bd83b9958f01c9c2d00bef6b1
Dimitris Gerasimou <gerasimou@ece.auth.gr>
2022-12-10 15:17:00 +0200

    Changed config.h layout

--- src/config.h
+++ src/config.h
@@ -4,25 +4,31 @@
 #define SESSION_FILE "/tmp/dwm-session"
 
 /* appearance */
-static const int swallowfloating         = 0;   /* 1 means swallow floating windows by default */
-static unsigned int borderpx             = 2;   /* border pixel of windows */
-static unsigned int snap                 = 32;  /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft  = 0;   /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray             = 1;   /* 0 means no systray */
-static int showbar                       = 1;   /* 0 means no bar */
-static int topbar                        = 1;   /* 0 means bottom bar */
-static const unsigned int statusbarlhpad = 0;   /* left horizontal padding of status bar */
-static const unsigned int showstatusallmon = 1; /* 1 means show statusbar on all monitors */
-static const unsigned int hidevacanttags = 1;   /* 0 means show all tags */
+
+/* bar */
+static const unsigned int systraypinning   = 0;    /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft    = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing   = 2;    /* systray spacing */
+static const int systraypinningfailfirst   = 1;    /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray               = 1;    /* 0: no systray */
+static int showbar                         = 1;    /* 0: no bar */
+static int topbar                          = 1;    /* 0: bottom bar */
+static const unsigned int statusbarlhpad   = 0;    /* right horizontal padding of status bar */
+static const unsigned int showstatusallmon = 1;    /* 1: show statusbar on all monitors */
+static const unsigned int hidevacanttags   = 1;    /* 1: hide vacant tags */
+
+/* windows */
+static const int swallowfloating    = 0;     /* 1: swallow floating windows by default */
+static unsigned int borderpx        = 2;     /* border pixel of windows */
+static unsigned int snap            = 32;    /* snap pixel */
 static const int focusonwheel       = 0;
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gappih    = 20;    /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;    /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;    /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;    /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 1;     /* 1: no outer gap when there is only one window */
+
+/* fonts and colors */
 static const char *fonts[]          = { "Fira Mono:style=Regular:size=11",
 										"JoyPixels:style=Regular:size=11:antialias=true",
 										"Hack Nerd Font Mono:style=Regular:size=11:antialias=true" , 
@@ -37,6 +43,25 @@ static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+};
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "normbgcolor",        STRING,  &normbgcolor },
+		{ "normbordercolor",    STRING,  &normbordercolor },
+		{ "normfgcolor",        STRING,  &normfgcolor },
+		{ "selbgcolor",         STRING,  &selbgcolor },
+		{ "selbordercolor",     STRING,  &selbordercolor },
+		{ "selfgcolor",         STRING,  &selfgcolor },
+		{ "borderpx",          	INTEGER, &borderpx },
+		{ "snap",          		INTEGER, &snap },
+		{ "showbar",          	INTEGER, &showbar },
+		{ "topbar",          	INTEGER, &topbar },
+		{ "nmaster",          	INTEGER, &nmaster },
+		{ "resizehints",       	INTEGER, &resizehints },
+		{ "mfact",      	 	FLOAT,   &mfact },
 };
 
 /* tagging */
@@ -98,42 +123,25 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
+
+/* application launch */
 static const char *dmenucmd[]   = { "dmenu_run", "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]    = { "st", NULL };
 static const char *browsercmd[] = { "firefox", NULL };
 static const char *emailcmd[]   = { "thunderbird", NULL };
 static const char *explrcmd[]   = { "dolphin", NULL };
 
-/*
- * Xresources preferences to load at startup
- */
-ResourcePref resources[] = {
-		{ "normbgcolor",        STRING,  &normbgcolor },
-		{ "normbordercolor",    STRING,  &normbordercolor },
-		{ "normfgcolor",        STRING,  &normfgcolor },
-		{ "selbgcolor",         STRING,  &selbgcolor },
-		{ "selbordercolor",     STRING,  &selbordercolor },
-		{ "selfgcolor",         STRING,  &selfgcolor },
-		{ "borderpx",          	INTEGER, &borderpx },
-		{ "snap",          		INTEGER, &snap },
-		{ "showbar",          	INTEGER, &showbar },
-		{ "topbar",          	INTEGER, &topbar },
-		{ "nmaster",          	INTEGER, &nmaster },
-		{ "resizehints",       	INTEGER, &resizehints },
-		{ "mfact",      	 	FLOAT,   &mfact },
-};
-
-/* Volume control */
+/* volume control */
 static const char *volumeup[]   = { CONFIGPATH(audiocontrol), "sink",     "increase",    NULL };
 static const char *volumedown[] = { CONFIGPATH(audiocontrol), "sink",     "decrease",    NULL };
 static const char *volumemute[] = { CONFIGPATH(audiocontrol), "sink",     "toggle-mute", NULL };
 static const char *micmute[]    = { CONFIGPATH(audiocontrol), "source",   "toggle-mute", NULL };
 
-/* Brightness control */
+/* brightness control */
 static const char *brightup[]   = { "brightnessctl", "--class=backlight", "set", "+5%", NULL };
 static const char *brightdown[] = { "brightnessctl", "--class=backlight", "set", "5%-", NULL };
 
-/* Keyboard language */
+/* keyboard language */
 static const char *switchlang[] = { CONFIGPATH(swaplanguage), NULL };
 
 /* keys */
