f80e8a37db76c9db04b99136bd67639e1f208a23
Dimitris Gerasimou <gerasimou@ece.auth.gr>
2023-03-11 06:13:35 +0200

    Added switchable layout symbol

--- dwm.c
+++ dwm.c
@@ -1138,9 +1138,12 @@ drawbar(Monitor *m)
 		}
 		x += w;
 	}
-	w = TEXTW(m->ltsymbol);
-	drw_setscheme(drw, scheme[SchemeNorm]);
-	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
+	
+	if(!hidelayout) {
+		w = TEXTW(m->ltsymbol);
+		drw_setscheme(drw, scheme[SchemeNorm]);
+		x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
+	}
 
 	if ((w = m->ww - tw - stw - x) > bh) {
 		if (m->sel) {
