78579378810c26bc22dde0c4f0ab3d571dd7c887
Dimitris Gerasimou <gerasimou@ece.auth.gr>
2023-04-25 23:08:26 +0300

    Added lock bind

--- config.h
+++ config.h
@@ -143,10 +143,13 @@ static const char *dmenuall[]   = { "dmenu_run", "-c", "-i", "-n", "-h", "12", "
 static const char *termcmd[]    = { "st", NULL };
 static const char *browsercmd[] = { "firefox", NULL };
 static const char *emailcmd[]   = { "thunderbird", NULL };
-static const char *explrcmd[]   = { "dolphin", NULL };
+static const char *explrcmd[]   = { "pcmanfm", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+static const char *emacscmd[] = { "emacsclient", "-c", "-a", "'emacs'", NULL };
 static const char *layoutmenu_cmd = CONFIGPATH(layoutmenu);
+static const char *lockcmd[] = { "slock", NULL };
+
 /* volume control */
 static const char *volumeup[]   = { CONFIGPATH(audiocontrol), "sink",     "increase",    NULL };
 static const char *volumedown[] = { CONFIGPATH(audiocontrol), "sink",     "decrease",    NULL };
@@ -180,7 +183,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_e,         spawn,          {.v = emailcmd } },
 	{ MODKEY,                       XK_r,         spawn,          {.v = explrcmd } },
 	{ MODKEY,                       XK_space,     spawn,          {.v = switchlang } },
+	{ MODKEY,                       XK_t,         spawn,          {.v = emacscmd } },
 	{ MODKEY,                       XK_grave,     togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                       XK_p,         spawn,          {.v = lockcmd } },
 
 	{ MODKEY,                       XK_b,         togglebar,      {0} },
 	{ MODKEY,                       XK_j,         focusstack,     {.i = +1 } },
@@ -194,9 +199,9 @@ static const Key keys[] = {
 /*	{ MODKEY,                       XK_Return,    zoom,           {0} }, */
 	{ MODKEY,                       XK_Tab,       view,           {0} },
 	{ MODKEY|ShiftMask,             XK_BackSpace, killclient,     {0} },
-	{ MODKEY,                       XK_t,         setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,         setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,         setlayout,      {.v = &layouts[2]} },
+	// { MODKEY,                       XK_t,         setlayout,      {.v = &layouts[0]} },
+	// { MODKEY,                       XK_f,         setlayout,      {.v = &layouts[1]} },
+	// { MODKEY,                       XK_m,         setlayout,      {.v = &layouts[2]} },
 	{ MODKEY|ControlMask,           XK_space,     setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,     togglefloating, {0} },
 	{ MODKEY,                       XK_0,         view,           {.ui = ~0 } },
