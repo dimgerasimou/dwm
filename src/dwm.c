c19e25a58b88795a4c6bef6c27e7cb58d77afa68
Dimitris Gerasimou <gerasimou-gh@posteo.gr>
2025-12-29 03:28:03 +0200

    [Fix] Suppress compiler warning
    (A function declaration without a prototype is deprecated in all versions of C)

--- src/dwm.c
+++ src/dwm.c
@@ -192,7 +192,7 @@ static void focusstack(const Arg *arg);
 static Atom getatomprop(Client *c, Atom prop);
 static int getrootptr(int *x, int *y);
 static long getstate(Window w);
-static unsigned int getsystraywidth();
+static unsigned int getsystraywidth(void);
 static int gettextprop(Window w, Atom atom, char *text, unsigned int size);
 static void grabbuttons(Client *c, int focused);
 static void grabkeys(void);
@@ -987,7 +987,7 @@ getatomprop(Client *c, Atom prop)
 }
 
 unsigned int
-getsystraywidth()
+getsystraywidth(void)
 {
 	unsigned int w = 0;
 	Client *i;
